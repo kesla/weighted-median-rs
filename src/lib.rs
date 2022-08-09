77d6eeeafc113c36d5c942195b8878cc1def3a4b
David Björklund <david.bjorklund@gmail.com>
2022-08-09 13:59:06 +0200

    hange data format

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,13 @@
 use is_sorted::IsSorted;
-use std::fmt::Debug;
 
-#[derive(Debug, PartialEq)]
-pub struct Data {
-    pub value: f64,
-    pub weight: f64,
-}
+// value, weight
+type Data = (f64, f64);
 
 fn weight_sum(input: &mut [Data], lower_weight_delta: f64, higher_weight_delta: f64) -> f64 {
     input
         .into_iter()
         .fold(lower_weight_delta + higher_weight_delta, |accum, item| {
-            accum + item.weight
+            accum + item.1
         })
 }
 
@@ -37,12 +33,12 @@ impl<'slice> WeightedMedian<'slice> {
 
         loop {
             let row = iterator.next().unwrap();
-            current_weight = current_weight + row.weight;
+            current_weight = current_weight + row.1;
 
             if current_weight / sum == 0.5 {
-                break (row.value + iterator.next().unwrap().value) / 2.0;
+                break (row.0 + iterator.next().unwrap().0) / 2.0;
             } else if current_weight / sum > 0.5 {
-                break row.value;
+                break row.0;
             }
         }
     }
@@ -51,14 +47,14 @@ impl<'slice> WeightedMedian<'slice> {
         let pivot_index = self.data.len() / 2;
         let (lower, pivot, higher) = self
             .data
-            .select_nth_unstable_by(pivot_index, |a, b| a.value.partial_cmp(&b.value).unwrap());
+            .select_nth_unstable_by(pivot_index, |a, b| a.0.partial_cmp(&b.0).unwrap());
 
         let lower_weight_sum = weight_sum(lower, self.lower_weight_delta, 0.0);
         let higher_weight_sum = weight_sum(higher, 0.0, self.higher_weight_delta);
-        let weight_sum = lower_weight_sum + pivot.weight + higher_weight_sum;
+        let weight_sum = lower_weight_sum + pivot.1 + higher_weight_sum;
 
         if lower_weight_sum / weight_sum < 0.5 && higher_weight_sum / weight_sum < 0.5 {
-            pivot.value
+            pivot.0
         } else if lower_weight_sum / weight_sum >= 0.5 {
             WeightedMedian::new(
                 &mut self.data[..pivot_index + 1],
@@ -80,18 +76,17 @@ impl<'slice> WeightedMedian<'slice> {
         let n = self.data.len();
 
         if n == 1 {
-            self.data[0].value
+            self.data[0].0
         } else if n == 2 {
-            if self.lower_weight_delta + self.data[0].weight
-                == self.data[1].weight + self.higher_weight_delta
+            if self.lower_weight_delta + self.data[0].1 == self.data[1].1 + self.higher_weight_delta
             {
-                (self.data[0].value + self.data[1].value) / 2.0
-            } else if self.data[0].weight > self.data[1].weight {
-                self.data[0].value
+                (self.data[0].0 + self.data[1].0) / 2.0
+            } else if self.data[0].1 > self.data[1].1 {
+                self.data[0].0
             } else {
-                self.data[1].value
+                self.data[1].0
             }
-        } else if IsSorted::is_sorted_by_key(&mut self.data.into_iter(), |data| data.value) {
+        } else if IsSorted::is_sorted_by_key(&mut self.data.into_iter(), |data| data.0) {
             self.calculate_sorted()
         } else {
             self.calculate_not_sorted()
@@ -105,83 +100,28 @@ pub fn weighted_median(input: &mut [Data]) -> f64 {
 
 #[cfg(test)]
 mod tests {
-    use crate::{weighted_median, Data};
+    use crate::weighted_median;
 
     #[test]
     fn one_element() {
-        assert_eq!(
-            weighted_median(&mut [Data {
-                value: 7.0,
-                weight: 9.0
-            }]),
-            7.0
-        );
+        assert_eq!(weighted_median(&mut [(7.0, 9.0)]), 7.0);
     }
 
     #[test]
     fn two_elements_different_weight() {
-        assert_eq!(
-            weighted_median(&mut [
-                Data {
-                    value: 7.0,
-                    weight: 1.0
-                },
-                Data {
-                    value: 8.0,
-                    weight: 2.0
-                }
-            ]),
-            8.0
-        );
-        assert_eq!(
-            weighted_median(&mut [
-                Data {
-                    value: 8.0,
-                    weight: 2.0
-                },
-                Data {
-                    value: 7.0,
-                    weight: 1.0
-                },
-            ]),
-            8.0
-        );
+        assert_eq!(weighted_median(&mut [(7.0, 1.0), (8.0, 2.0)]), 8.0);
+        assert_eq!(weighted_median(&mut [(8.0, 2.0), (7.0, 1.0),]), 8.0);
     }
 
     #[test]
     fn two_elements_same_weight() {
-        assert_eq!(
-            weighted_median(&mut [
-                Data {
-                    value: 7.0,
-                    weight: 1.0
-                },
-                Data {
-                    value: 8.0,
-                    weight: 1.0
-                }
-            ]),
-            7.5
-        )
+        assert_eq!(weighted_median(&mut [(7.0, 1.0), (8.0, 1.0)]), 7.5)
     }
 
     #[test]
     fn three_elements_is_first_element() {
         assert_eq!(
-            weighted_median(&mut [
-                Data {
-                    value: 2.0,
-                    weight: 1.0
-                },
-                Data {
-                    value: 3.0,
-                    weight: 1.0
-                },
-                Data {
-                    value: 1.0,
-                    weight: 1.0
-                },
-            ]),
+            weighted_median(&mut [(2.0, 1.0), (3.0, 1.0), (1.0, 1.0),]),
             2.0
         )
     }
@@ -189,20 +129,7 @@ mod tests {
     #[test]
     fn three_elements_is_middle_element() {
         assert_eq!(
-            weighted_median(&mut [
-                Data {
-                    value: 3.0,
-                    weight: 1.0
-                },
-                Data {
-                    value: 2.0,
-                    weight: 1.0
-                },
-                Data {
-                    value: 1.0,
-                    weight: 1.0
-                },
-            ]),
+            weighted_median(&mut [(3.0, 1.0), (2.0, 1.0), (1.0, 1.0),]),
             2.0
         )
     }
@@ -210,20 +137,7 @@ mod tests {
     #[test]
     fn three_elements_is_last_element() {
         assert_eq!(
-            weighted_median(&mut [
-                Data {
-                    value: 3.0,
-                    weight: 1.0
-                },
-                Data {
-                    value: 1.0,
-                    weight: 1.0
-                },
-                Data {
-                    value: 2.0,
-                    weight: 1.0
-                },
-            ]),
+            weighted_median(&mut [(3.0, 1.0), (1.0, 1.0), (2.0, 1.0),]),
             2.0
         )
     }
@@ -231,20 +145,7 @@ mod tests {
     #[test]
     fn three_elements_is_smallest_element() {
         assert_eq!(
-            weighted_median(&mut [
-                Data {
-                    value: 3.0,
-                    weight: 1.0
-                },
-                Data {
-                    value: 2.0,
-                    weight: 1.0
-                },
-                Data {
-                    value: 1.0,
-                    weight: 5.0
-                },
-            ]),
+            weighted_median(&mut [(3.0, 1.0), (2.0, 1.0), (1.0, 5.0),]),
             1.0
         )
     }
@@ -252,20 +153,7 @@ mod tests {
     #[test]
     fn three_elements_is_biggest_element() {
         assert_eq!(
-            weighted_median(&mut [
-                Data {
-                    value: 3.0,
-                    weight: 5.0
-                },
-                Data {
-                    value: 2.0,
-                    weight: 1.0
-                },
-                Data {
-                    value: 1.0,
-                    weight: 1.0
-                },
-            ]),
+            weighted_median(&mut [(3.0, 5.0), (2.0, 1.0), (1.0, 1.0),]),
             3.0
         )
     }
@@ -273,37 +161,11 @@ mod tests {
     #[test]
     fn three_elements_is_even() {
         assert_eq!(
-            weighted_median(&mut [
-                Data {
-                    value: 3.0,
-                    weight: 2.0,
-                },
-                Data {
-                    value: 2.0,
-                    weight: 1.0,
-                },
-                Data {
-                    value: 1.0,
-                    weight: 1.0,
-                },
-            ]),
+            weighted_median(&mut [(3.0, 2.0,), (2.0, 1.0,), (1.0, 1.0,),]),
             2.5
         );
         assert_eq!(
-            weighted_median(&mut [
-                Data {
-                    value: 1.0,
-                    weight: 1.0,
-                },
-                Data {
-                    value: 2.0,
-                    weight: 1.0,
-                },
-                Data {
-                    value: 3.0,
-                    weight: 2.0,
-                },
-            ]),
+            weighted_median(&mut [(1.0, 1.0,), (2.0, 1.0,), (3.0, 2.0,),]),
             2.5
         );
     }
@@ -311,24 +173,7 @@ mod tests {
     #[test]
     fn four_elements_is_even() {
         assert_eq!(
-            weighted_median(&mut [
-                Data {
-                    value: 1.0,
-                    weight: 0.49,
-                },
-                Data {
-                    value: 2.0,
-                    weight: 0.01,
-                },
-                Data {
-                    value: 3.0,
-                    weight: 0.25,
-                },
-                Data {
-                    value: 1000.0,
-                    weight: 0.25,
-                },
-            ]),
+            weighted_median(&mut [(1.0, 0.49,), (2.0, 0.01,), (3.0, 0.25,), (1000.0, 0.25),]),
             2.5
         );
     }
@@ -336,72 +181,23 @@ mod tests {
     #[test]
     fn five_elements_is_pivot_value() {
         assert_eq!(
-            weighted_median(&mut [
-                Data {
-                    value: 2.0,
-                    weight: 0.5
-                },
-                Data {
-                    value: 1.0,
-                    weight: 0.5
-                },
-                Data {
-                    value: 3.0,
-                    weight: 1.0
-                },
-                Data {
-                    value: 10.0,
-                    weight: 0.8
-                },
-                Data {
-                    value: 8.0,
-                    weight: 0.2
-                }
-            ]),
+            weighted_median(&mut [(2.0, 0.5), (1.0, 0.5), (3.0, 1.0), (10.0, 0.8), (8.0, 0.2)]),
             3.0
         );
     }
 
     mod sorted {
-        use crate::{Data, WeightedMedian};
+        use crate::{WeightedMedian};
 
         #[test]
         fn with_lower_and_higher_delta() {
             assert_eq!(
-                WeightedMedian::new(
-                    &mut [
-                        Data {
-                            value: 1.0,
-                            weight: 1.0
-                        },
-                        Data {
-                            value: 2.0,
-                            weight: 2.0
-                        }
-                    ],
-                    1.0,
-                    0.0
-                )
-                .calculate_sorted(),
+                WeightedMedian::new(&mut [(1.0, 1.0), (2.0, 2.0)], 1.0, 0.0).calculate_sorted(),
                 1.5
             );
 
             assert_eq!(
-                WeightedMedian::new(
-                    &mut [
-                        Data {
-                            value: 1.0,
-                            weight: 2.0
-                        },
-                        Data {
-                            value: 2.0,
-                            weight: 1.0
-                        }
-                    ],
-                    0.0,
-                    1.0
-                )
-                .calculate_sorted(),
+                WeightedMedian::new(&mut [(1.0, 2.0), (2.0, 1.0)], 0.0, 1.0).calculate_sorted(),
                 1.5
             )
         }
