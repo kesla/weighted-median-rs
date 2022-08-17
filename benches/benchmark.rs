538ea607567db3a9d50b33356c91d91fbf1aeef9
David Björklund <david.bjorklund@gmail.com>
2022-08-17 16:18:14 +0200

    bench: separate tests for sorted & unsorted data

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -15,23 +15,31 @@ impl Data for TestData {
         self.weight
     }
 }
-fn generate_test_data() -> Vec<TestData>{
+fn generate_test_data<F: Fn(usize) -> (usize, usize)>(func: F) -> Vec<TestData> {
     let mut data = Vec::<TestData>::new();
 
     for count in 0..100 {
+        let (weight, value) = func(count);
         data.push(TestData {
-            weight: (count % 19) as f64,
-            value: ((count * 119) % 129) as f64,
+          weight: weight as f64,
+          value: value as f64
         });
     }
     data
 }
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-  let test_data = &mut generate_test_data()[..];
+    let unsorted_test_data = &mut generate_test_data(|count| (count % 19, (count * 119) % 129))[..];
+    let sorted_test_data = &mut generate_test_data(|count| (count % 2, count))[..];
 
+    c.bench_function("weighted_median - unsorted", |b| {
+        b.iter(|| weighted_median(black_box(unsorted_test_data)))
+    });
 
-  c.bench_function("weighted_median", |b| b.iter(|| weighted_median(black_box(test_data))));
+    c.bench_function("weighted_median - sorted", |b| {
+      b.iter(|| weighted_median(black_box(sorted_test_data)))
+  });
+
 }
 
 criterion_group!(benches, criterion_benchmark);
